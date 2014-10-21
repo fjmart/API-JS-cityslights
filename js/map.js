0de75a6b76d074e6dc3c5c6b5f9c318faf01d8cb
Francisco J. Martin <fjmscg@gmail.com>
2014-10-21 20:52:22 +0200

    Style Map

--- js/map.js
+++ js/map.js
@@ -33,19 +33,68 @@ function HomeControl(controlDiv, map) {
   });
   console.log($('#home'));
    controlUI.innerHTML = '<div class="panel panel-default leftpanel">'+'<div class="panel-heading">Panel heading</div><div class="panel-body"><form action="index.html" method="post"><fieldset><h3>Filters</h3><label for="power">POWER</label><br><input type="range" id="power" name="filter_power"><br><label for="time">HOURS/DAY</label><br><input type="range" id="time" name="filter_time"><br><label for="age">AGE</label><br><input type="range" id="age" name="filter_age"></fieldset><fieldset><h3>Remark</h3><input type="checkbox" id="led" name="remark_led"><label for="lef">LED</label><br><input type="checkbox" id="heatmap" name="remark_heatmap"><label for="heatmap">HEATMAP</label><br><input type="checkbox" id="old" name="remark_old"><label for="old">OLD</label></fieldset></form></div></div>';
+
+
+
+
 }
 
 
 
 function initialize() {
+  // Create an array of styles.
+  var styles = [
+  {
+    "elementType": "geometry.fill",
+    "stylers": [
+      { "color": "#5c8080" }
+    ]
+  },{
+    "featureType": "road",
+    "elementType": "geometry.fill",
+    "stylers": [
+      { "hue": "#e6ff00" }
+    ]
+  },{
+    "featureType": "road.local",
+    "elementType": "geometry.fill",
+    "stylers": [
+      { "hue": "#f6ff00" },
+      { "lightness": 41 },
+      { "saturation": 23 }
+    ]
+  },{
+    "featureType": "road.arterial",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      { "saturation": 23 },
+      { "lightness": 41 },
+      { "hue": "#f6ff00" }
+    ]
+  },{
+    "featureType": "road.highway.controlled_access",
+    "elementType": "geometry.fill"  },{
+  }
+]
+// Create a new StyledMapType object, passing it the array of styles,
+  // as well as the name to be displayed on the map type control.
+  var styledMap = new google.maps.StyledMapType(styles,
+    {name: "Styled Map"});
   var mapOptions = {
     zoom: 8,
     disableDefaultUI: true,
+    mapTypeControlOptions: {
+      mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+    },
     center: new google.maps.LatLng(-34.397, 150.644)
   };
 
   var map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
+
+  //Associate the styled map with the MapTypeId and set it to display.
+  map.mapTypes.set('map_style', styledMap);
+  map.setMapTypeId('map_style');
 
   // Create the DIV to hold the control and call the HomeControl() constructor
   // passing in this DIV.
