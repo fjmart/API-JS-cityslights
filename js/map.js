e1e18df6bfc71c43623ae969f41102473641c395
Francisco J. Martin <fjmscg@gmail.com>
2014-10-21 19:53:24 +0200

    Revert "Revert b651a1d..05448c9"
    This reverts commit 6aeac49c75b5addac236cf575d1a7280c12a877a.

--- js/map.js
+++ js/map.js
@@ -18,24 +18,16 @@ function HomeControl(controlDiv, map) {
   // from the edge of the map.
   controlDiv.style.padding = '5px';
 
-  // Set CSS for the control border.
-  var controlUI = document.createElement('div');
-  controlUI.style.backgroundColor = 'white';
-  controlUI.style.borderStyle = 'solid';
-  controlUI.style.borderWidth = '2px';
-  controlUI.style.cursor = 'pointer';
-  controlUI.style.textAlign = 'center';
-  controlUI.title = 'Click to set the map to Home';
-  controlDiv.appendChild(controlUI);
+ 
 
   // Set CSS for the control interior.
-  var controlText = document.createElement('div');
-  controlText.style.fontFamily = 'Arial,sans-serif';
-  controlText.style.fontSize = '12px';
-  controlText.style.paddingLeft = '4px';
-  controlText.style.paddingRight = '4px';
-  controlText.innerHTML = '<strong>Home</strong>';
-  controlUI.appendChild(controlText);
+  var controlUI = document.createElement('div');
+  controlUI.style.fontFamily = 'Arial,sans-serif';
+  controlUI.style.fontSize = '12px';
+  controlUI.style.paddingLeft = '4px';
+  controlUI.style.paddingRight = '4px';
+  controlUI.innerHTML = '<button type="button" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-th"></span></button>';
+  controlDiv.appendChild(controlUI);
 
   // VARIABLES
 
