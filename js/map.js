d7c01c01f6cadf4cc9f1d1c92a5d49894a631aa1
Francisco J. Martin <fjmscg@gmail.com>
2014-10-22 18:52:35 +0200

    multiple controls
    home control is duplicated to graphs control

--- js/map.js
+++ js/map.js
@@ -1,4 +1,7 @@
+<script type="text/html"> 
 
+<div class="panel panel-default leftpanel">'+'<div class="panel-heading">Panel heading</div><div class="panel-body"><form action="index.html" method="post"><fieldset><h3>Filters</h3><label for="power">POWER</label><br><input type="range" id="power" name="filter_power"><br><label for="time">HOURS/DAY</label><br><input type="range" id="time" name="filter_time"><br><label for="age">AGE</label><br><input type="range" id="age" name="filter_age"></fieldset><fieldset><h3>Remark</h3><input type="checkbox" id="led" name="remark_led"><label for="lef">LED</label><br><input type="checkbox" id="heatmap" name="remark_heatmap"><label for="heatmap">HEATMAP</label><br><input type="checkbox" id="old" name="remark_old"><label for="old">OLD</label></fieldset></form></div></div>
+</script>
 
 
 // Construct your control in whatever manner is appropriate.
@@ -17,7 +20,7 @@ function HomeControl(controlDiv, map) {
   // Setting padding to 5 px will offset the control
   // from the edge of the map.
   controlDiv.style.padding = '25px';
-  controlDiv.id="home";
+
   // Set CSS for the control border.
   var controlUI = document.createElement('div');
   controlUI.title = 'Click to set the map to Home';
@@ -31,8 +34,8 @@ function HomeControl(controlDiv, map) {
   google.maps.event.addDomListener(controlUI, 'click', function() {
     map.setCenter(chicago)
   });
-  console.log($('#home'));
-   controlUI.innerHTML = '<div class="panel panel-default leftpanel">'+'<div class="panel-heading">Panel heading</div><div class="panel-body"><form action="index.html" method="post"><fieldset><h3>Filters</h3><label for="power">POWER</label><br><input type="range" id="power" name="filter_power"><br><label for="time">HOURS/DAY</label><br><input type="range" id="time" name="filter_time"><br><label for="age">AGE</label><br><input type="range" id="age" name="filter_age"></fieldset><fieldset><h3>Remark</h3><input type="checkbox" id="led" name="remark_led"><label for="lef">LED</label><br><input type="checkbox" id="heatmap" name="remark_heatmap"><label for="heatmap">HEATMAP</label><br><input type="checkbox" id="old" name="remark_old"><label for="old">OLD</label></fieldset></form></div></div>';
+
+   controlUI.innerHTML = '<div class="panel panel-default leftpanel"><div class="panel-heading">Panel heading</div><div class="panel-body"><form action="index.html" method="post"><fieldset><h3>Filters</h3><label for="power">POWER</label><br><input type="range" id="power" name="filter_power"><br><label for="time">HOURS/DAY</label><br><input type="range" id="time" name="filter_time"><br><label for="age">AGE</label><br><input type="range" id="age" name="filter_age"></fieldset><fieldset><h3>Remark</h3><input type="checkbox" id="led" name="remark_led"><label for="lef">LED</label><br><input type="checkbox" id="heatmap" name="remark_heatmap"><label for="heatmap">HEATMAP</label><br><input type="checkbox" id="old" name="remark_old"><label for="old">OLD</label></fieldset></form></div></div>';
 
 
 
@@ -103,7 +106,12 @@ function initialize() {
   homeControlDiv.index = 1;
   map.controls[google.maps.ControlPosition.TOP_RIGHT].push(homeControlDiv);
 
-
+ // Create the DIV to hold the control and call the HomeControl() constructor
+  // passing in this DIV.
+  var graphsControlDiv = document.createElement('div');
+  var graphsControlDiv = new graphsControl(graphsControlDiv, map);
+  graphsControlDiv.index = 1;
+  map.controls[google.maps.ControlPosition.TOP_RIGHT].push(graphsControlDiv);
 
 
 }
